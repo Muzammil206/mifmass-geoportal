9907427503a4ddcf0c7224a38b659b8785a134a9
muzammil206 <ismailmuzammil206@gmail.com>
2025-11-15 13:26:03 +0100

    fixed

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -3,7 +3,6 @@ import HeroSection from "@/components/hero-section"
 import FeaturesSection from "@/components/features-section"
 import FooterSection from "@/components/footer-section"
 
-export const dynamic = "force-dynamic"
 
 export default function HomePage() {
   return (
